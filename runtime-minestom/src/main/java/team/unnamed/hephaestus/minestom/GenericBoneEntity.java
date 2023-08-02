df4be5d9cfea6fb42149bda7b8518d3a53b5be9a
Connor Reinholdtsen <43483846+connorrein@users.noreply.github.com>
2023-08-01 22:05:41 -0700

    Make bones autoviewable

--- runtime-minestom/src/main/java/team/unnamed/hephaestus/minestom/GenericBoneEntity.java
+++ runtime-minestom/src/main/java/team/unnamed/hephaestus/minestom/GenericBoneEntity.java
@@ -33,8 +33,6 @@ abstract class GenericBoneEntity extends EntityCreature implements BaseBoneView 
 
     public GenericBoneEntity(@NotNull EntityType entityType) {
         super(entityType);
-        // bones are not auto-viewable by default
-        setAutoViewable(false);
     }
 
     abstract void colorize(Color color);
